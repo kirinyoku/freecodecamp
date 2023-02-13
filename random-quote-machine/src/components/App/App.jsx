16f14cbef787ecf05fb31bfd5345f1e418424dad
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-02-13 10:58:03 +0200

    Update

--- random-quote-machine/src/components/App/App.jsx
+++ random-quote-machine/src/components/App/App.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import QuoteBox from '../QuoteBox/QuoteBox'
+import QuoteService from '../../services/QuoteService';
+import { useState, useEffect } from 'react';
+
+import './App.scss';
 
 export default function App() {
+
+  const [ quote, setQuote ] = useState('');
+  const [ auth, setAuth ] = useState('');
+  const [ color, setColor ] = useState ('');
+
+  const { getRandomQuote } = QuoteService();
+
+  const getRandomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    if (parseInt(color.substring(1), 16) > 0xffffff/2) {
+      return getRandomColor();
+    } else {
+      return color;
+    }
+  }
+
+  const newQuote = () => {
+    getRandomQuote().then(res => {
+      setQuote(res.quote);
+      setAuth(res.auth);
+      setColor(getRandomColor());
+    });
+  }
+
+  useEffect(() => {
+    newQuote();
+  }, [])
+
   return (
-    <div>
-      <QuoteBox />
+    <div className='app' style={{backgroundColor: color}}>
+      <QuoteBox quote={{text: quote, auth, color}} newQuote={newQuote} />
     </div>
   )
 }
--- random-quote-machine/src/components/QuoteBox/QuoteBox.jsx
+++ random-quote-machine/src/components/QuoteBox/QuoteBox.jsx
@@ -1,70 +1,37 @@
 import React from 'react';
-import QuoteService from '../../services/QuoteService';
-import { useState, useEffect } from 'react';
 import './QuoteBox.scss';
 
-export default function QuoteBox() {
-
-  const [ quote, setQuote ] = useState('');
-  const [ auth, setAuth ] = useState('');
-  const [ color, setColor ] = useState ('');
-
-  const { getRandomQuote } = QuoteService();
-  
-  useEffect(() => {
-    newQuote();
-  }, [])
-
-  const getRandomColor = () => {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    if (parseInt(color.substring(1), 16) > 0xffffff/2) {
-      return getRandomColor();
-    } else {
-      return color;
-    }
-  }
-
-  const newQuote = () => {
-    getRandomQuote().then(res => {
-      setQuote(res.quote);
-      setAuth(res.auth);
-      setColor(getRandomColor());
-    });
-  }
+export default function QuoteBox( { quote, newQuote } ) {
 
   const buttonHandler = () => {
     newQuote();
   }
 
   return (
-    <div className='quote' id='quote-box' style={{color: color}}>
+    <div className='quote' id='quote-box' style={{color: quote.color}}>
       <figure>
         <blockquote id="text">
           <i className='fa fa-quote-left'></i>
-          <span className='quote__text'>{quote}</span>
+          <span className='quote__text'>{quote.text}</span>
         </blockquote>
         <figcaption className='quote__author' id="author">
-          &mdash; {auth ?? 'Beautiful Person'}
+          &mdash; {quote.auth ?? 'Beautiful Person'}
         </figcaption>
       </figure>
       <footer className='quote__footer'>
         <ul className='quote__social-links' aria-label='social link list'>
           <li aria-label='twitter'>
-            <a className='quote__link' href="https://twitter.com" id="tweet-quote" rel="noreferrer" target="_blank">
-              <i className="fa fa-twitter icon" style={{backgroundColor: color}}></i>
+            <a className='quote__link' href={`https://twitter.com/intent/tweet?hashtags=quotes&text=\"${quote.text}\" ${quote.auth}`} id="tweet-quote" rel="noreferrer" target="_blank">
+              <i className="fa fa-twitter icon" style={{backgroundColor: quote.color}}></i>
             </a>
           </li>
-          <li aria-label='facebook' id="facebook-quote" rel="noreferrer" target="_blank">
-            <a className='quote__link' href="https://facebook.com">
-              <i className='fa fa-tumblr icon' style={{backgroundColor: color}}></i>
+          <li aria-label='tumblr' id="tumblr-quote">
+            <a className='quote__link' href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${quote.auth}&content=\"${quote.text}\"&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`} rel="noreferrer" target="_blank">
+              <i className='fa fa-tumblr icon' style={{backgroundColor: quote.color}}></i>
             </a>
           </li>
         </ul>
-        <button onClick={buttonHandler} className='quote__button' style={{backgroundColor: color}} type='button' id="new-quote">New quote</button>
+        <button onClick={buttonHandler} className='quote__button' style={{backgroundColor: quote.color}} type='button' id="new-quote">New quote</button>
       </footer>
     </div>
   )
